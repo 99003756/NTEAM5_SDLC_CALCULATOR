9def85e292feded7976c7ef6f4e04bc41528b201
99003756 <a.aruna@ltts.com>
2021-02-15 00:08:46 +0530

    changes made in files
     by aruna

--- 3_Implementation/project_main.c
+++ 3_Implementation/project_main.c
@@ -1,95 +1,33 @@
-#include "code.h"
+#include <code.h>
 
-/* Status of the operation requested */
-#define VALID   (1)
-#define INVALID (0)
-
-/* Calculator operation requested by user*/
-unsigned int calculator_operation = 0;
-
-/* Operands on which calculation is performed */
-int calculator_operand1 = 0;
-int calculator_operand2 = 0;
-
-/* Valid operations */
-enum operations{ AREA_CALCULATION=1,VOLUME_CALCULATION, EXIT };
-
-/* Display the menu of operations supported */
-void calculator_menu(void);
-/* Verifies the requested operations validity */
-int valid_operation(int operation);
-
-
-/* Start of the application */
-int main(int argc, char *argv[])
+int main()
 {
-    printf("\n****Calculator****\n");
-    while(1)
+    int ch;
+    do
     {
-        calculator_menu();
-    }
+            //printf("Enter choice 1->Arithmetic operations\n 2->Relational operations\n 3-
+            //>Logical operationss
+            printf("1->Area calculation\n 2->Volume calculation\n");
+            // 6->Factorial\n");
+            scanf("%d",&ch);
+
+            switch(ch)
+            {/*
+                case 1:Arithmetic_operations();
+                        break;
+                case 2:Relational_operations();
+                        break;
+                case 3:Logical_operations();
+                        break;*/
+             case 1:Area_calculation();
+                        break;
+                case 2:Volume_calculation();
+                        break;
+                //case 6:Factorial_calculations();
+                  //      break;
+                default:printf("Do nothing\n");
+            }
+    }while(ch!=3);
+    return 0;
 }
 
-void calculator_menu(void)
-{
-    printf("\nAvailable Operations\n");
-    printf("\n1. Area\n2. Volume\n3. Exit");
-    printf("\n\tEnter your choice\n");
-   
-     // __fpurge(stdin);
-    scanf("%d", &calculator_operation);
-
-    if(EXIT == calculator_operation)
-    {
-        printf("\nThank you. Exiting the Application\n");
-        exit(0);
-    }
-
-    if(INVALID != valid_operation(calculator_operation))
-    {
-        printf("\n\tEnter your Numbers with space between them\n");
-        // __fpurge(stdin);
-        scanf("%d %d", &calculator_operand1, &calculator_operand2);
-    }
-    else
-    {
-        printf("\n\t---Wrong choice---\nEnter to continue\n");
-        // __fpurge(stdin);
-        getchar();
-        return;
-        
-    }
-    switch(calculator_operation)
-    {
-        case AREA_CALCULATION:
-            printf("\n\t%d + %d = %d\nEnter to continue", 
-            calculator_operand1, 
-            calculator_operand2,
-            Area_calculation(calculator_operand1, calculator_operand2));
-            
-            // __fpurge(stdin);
-            getchar();
-            break;
-        case VOLUME_CALCULATION:
-            printf("\n\t%d - %d = %d\nEnter to continue", 
-            calculator_operand1, 
-            calculator_operand2,
-            Volume_calculation(calculator_operand1, calculator_operand2));
-            
-            // __fpurge(stdin);
-            getchar();
-            break;
-        
-        case 3:
-            exit(0);
-            break;
-        default:
-            printf("\n\t---It should never come here---\n");
-    }
-}
-
-int valid_operation(int operation)
-{
-    /* Check if the operation is a valid operation */
-    return ((AREA_CALCULATION <= operation) && (EXIT >= operation)) ? VALID: INVALID;
-}
