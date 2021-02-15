8937330a194cbb60ed016626dd3f349d1fd0ecc3
99003763 <78864900+99003763@users.noreply.github.com>
2021-02-15 14:26:04 +0530

    Add files via upload
    Commited by Rajat

---
+++ .github/workflows/trigno.h
@@ -0,0 +1,37 @@
+/** 
+* @file calculator_operations.h
+* Calculator application with 4 mathematical operations
+*
+*/
+#ifndef __CALCULATOR_OPERATIONS_H__
+#define __CALCULATOR_OPERATIONS_H__
+
+#include <stdio.h>
+#include <stdlib.h>
+#include<math.h>
+
+
+
+
+
+void Sin_function(double Degrees_Value);
+
+
+
+void Cos_function(double Degrees_Value);
+
+
+
+void Tan_function(double Degrees_Value);
+
+
+
+
+void Cosec_function(double Degrees_Value);
+
+void Sec_function(double Degrees_Value);
+
+
+void Cot_function(double Degrees_Value);
+
+#endif  /* #define __CALCULATOR_OPERATIONS_H__ */
