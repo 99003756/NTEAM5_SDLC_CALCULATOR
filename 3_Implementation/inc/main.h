4f9b234f31d3139d544ea0a0420967e2c4bc8b9f
99003756 <a.aruna@ltts.com>
2021-03-11 15:38:50 +0530

    main.c function prototypes added by aruna

--- 3_Implementation/inc/main.h
+++ 3_Implementation/inc/main.h
@@ -11,3 +11,16 @@ int Cosec_function(double Degrees_Value);
 int Sec_function(double Degrees_Value);
 int Cot_function(double Degrees_Value);
 
+
+// area calculations
+double area_circle(int num1);
+int area_rectangle(int num1,int num2);
+int area_square(int num1);
+double area_triangle(int num1,int num2);
+
+// volume calculations
+double vol_cube(int num1);
+int vol_cylinder(int num1,int num2);
+int vol_cone(int num1,int num2);
+int vol_sphere(int num1);
+
