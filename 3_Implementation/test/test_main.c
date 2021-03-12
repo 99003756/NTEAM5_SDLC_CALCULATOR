8ea5f868686fa11bfd50ff87898ca46d3276a607
99003756 <78857426+99003756@users.noreply.github.com>
2021-03-12 21:53:08 +0530

    Update test_main.c

--- 3_Implementation/test/test_main.c
+++ 3_Implementation/test/test_main.c
@@ -7,42 +7,6 @@ void setUp(){}
 
 void tearDown(){}
 
-//Test Case For Trignomatry
-void test_Sin_function(void);
-void test_Cos_function(void);
-void test_Tan_function(void);
-void test_Cosec_function(void);
-void test_Sec_function(void);
-void test_Cot_function(void);
-void test_exponential(void);
-void test_factorial(void);
-
-void test_Sin_function(void)
-{
-     TEST_ASSERT_EQUAL(0.5,Sin_function(30));
-}
-
-void test_Cos_function(void)
-{
-     TEST_ASSERT_EQUAL(0.5,Cos_function(30));
-}
-void test_Tan_function(void)
-{
-      TEST_ASSERT_EQUAL(-6,Tan_function(30));
-}
-void test_Cosec_function(void)
-{
-      TEST_ASSERT_EQUAL(-1,Cosec_function(30));
-}
-void test_Sec_function(void)
-{
-      TEST_ASSERT_EQUAL(0.5,Sec_function(30));
-}
-void test_Cot_function(void)
-{
-      TEST_ASSERT_EQUAL(0.5,Cot_function(30));
-}
-
 // test cases for area functions
 void test_cir(void)
 {
