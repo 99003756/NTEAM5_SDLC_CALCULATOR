c9314c0012b58fb36b38387ea18ae2d1b3534fdd
99003756 <a.aruna@ltts.com>
2021-03-12 17:33:05 +0530

    Separate header file created by Aruna

---
+++ 3_Implementation/inc/area_volume.h
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<math.h>
+
+#define PI 3.142
+
+// area calculations
+double area_circle(int num1);
+int area_rectangle(int num1,int num2);
+int area_square(int num1);
+double area_triangle(int num1,int num2);
+
+// volume calculations
+double vol_cube(int num1);
+int vol_cylinder(int num1,int num2);
+int vol_cone(int num1,int num2);
+int vol_sphere(int num1);
--- 3_Implementation/inc/main.h
+++ 3_Implementation/inc/main.h
@@ -12,25 +12,13 @@ int Sec_function(double Degrees_Value);
 int Cot_function(double Degrees_Value);
 
 
-// area calculations
-double area_circle(int num1);
-int area_rectangle(int num1,int num2);
-int area_square(int num1);
-double area_triangle(int num1,int num2);
-
-// volume calculations
-double vol_cube(int num1);
-int vol_cylinder(int num1,int num2);
-int vol_cone(int num1,int num2);
-int vol_sphere(int num1);
-
 //Arithmetic operations
-
+/*
 int add(int num1,int num2);
 int sub(int num1, int num2);
 int mul(int num1, int num2);
 int div(int num1,int num2);
 int sqrroot(int num1);
 int sqr(int num1);
-
+*/
 
