3d8d3ba5f30171b1bece060dee8e66173276e73d
99003758 <78871909+99003758@users.noreply.github.com>
2021-03-13 06:36:26 +0530

    done

--- 3_Implementation/src/factorial.c
+++ 3_Implementation/src/factorial.c
@@ -5,7 +5,7 @@
 // exponential will have only two inputs i had choosen the input types as floats so it will take all values
 float Exponential(float num1,float num2)
 {
-    float result,t;
+    float result,t=1;
     result=pow(num1,num2);
     for(int i=1;i<=num2;i++)
     {
